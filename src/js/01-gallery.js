aeeb813041e53f33a2dbd4f81eddf8a82f875e05
Andreii-Denysenko <a.denisenko2111@gmail.com>
2022-08-07 17:42:54 +0300

    solution tack 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+
+const galleryContainer = document.querySelector('.gallery');
+const addMurkup = createImageMurkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", addMurkup);
+
+function createImageMurkup(galleryItems) {
+return galleryItems
+.map(({preview, original, description})=>{
+    return`
+    <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+  </a>`;
+})
+.join("");
+};
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionType: "attr",
+    captionPosition: "bottom",
+    captionDelay: 250,
+    captionsData: "alt",  
+ });
 console.log(galleryItems);
